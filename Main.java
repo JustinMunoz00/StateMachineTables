8d78b0dd0d0e2b47d2121818abe446d6080e46f6
JustinMunoz00 <justinm880@nycstudents.net>
2025-06-08 03:48:35 +0000

    Can convert truth tables into kmap representation but they are not solved. Only server as a guide for user.

--- Main.java
+++ Main.java
@@ -8,9 +8,17 @@ public class Main {
         System.out.println("What 2 words are you trying to represent?");
         arr.add(sc.nextLine());
         arr.add(sc.nextLine());
-        StateTable t = new StateTable(arr)
+        StateTable t = new StateTable(arr);
         t.printChars();
         t.dValues();
+        t.ckInputs();
         t.results();
+        System.out.println();
+        t.printCkInputs();
+        System.out.println();
+        System.out.println("Do you want the kmap representations? (only available for state machines with more than 3 states and less than 9)");
+        if (sc.nextLine().contains("y")){
+            t.kmaps();
+        } else sc.close();
         }
     }
